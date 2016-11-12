b6d72b223dd0c6d4d72f1828360dac053d6bcd3d
Rubest <ruban_hussain@brown.edu>
2016-11-12 00:29:39 -0500

    Created view for calendar adding and added a sequel to it

---
+++ Posterpret/AddCalendarEventViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddCalendarEventViewController.swift
+//  Posterpret
+//
+//  Created by Ruban Hussain on 11/12/16.
+//  Copyright © 2016 Ruban Hussain. All rights reserved.
+//
+
+import UIKit
+
+class AddCalendarEventViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
+
