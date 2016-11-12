9dada6ed66826dfef03a5c1b3474c23c2c4209f5
noahhashmi <noahhashmi@gmail.com>
2016-11-12 12:02:29 -0500

    can now post requests to google cloud vision API

--- Posterpret/ViewController.swift
+++ Posterpret/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var addImg: UIButton!
     @IBOutlet weak var takePic: UIButton!
+    @IBOutlet weak var apiButton: UIButton!
     
     var imagePicker: UIImagePickerController!
     
@@ -40,5 +41,89 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.navigationController?.pushViewController(imagePicker, animated: true)
         takePic.setTitle("", forState: .Normal)
     }
+    
+    
+    @IBAction func apiButton(sender: AnyObject) {
+        let jsonRequest: [String: AnyObject] = [
+            "requests": [
+                "features": [
+                "type":"LABEL_DETECTION"
+            ],
+            "image": [
+            "source": [
+            "gcsImageUri":"gs://bucket/demo-image.jpg"
+        ]
+        ]
+        ]
+        ]
+        
+        data_request("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDcXIushxfZ3Bf2Dl-MDxVitmBw0hE8LBE", jsonObject: jsonToString(jsonRequest));
+    }
+    
+    func data_request(url_to_request: String, jsonObject: String) {
+        
+        let json = [
+            "requests": [
+                "features": [
+                "type":"TEXT_DETECTION"
+            ],
+            "image": [
+            "source": [
+            "gcsImageUri":"gs://posterpret-buck/poster.png"
+        ]
+        ]
+        ]
+        ]
+        
+        let data : NSData = NSKeyedArchiver.archivedDataWithRootObject(json)
+        
+        do {
+            
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(json, options: .PrettyPrinted)
+            
+            // create post request
+            let url = NSURL(string: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDcXIushxfZ3Bf2Dl-MDxVitmBw0hE8LBE")!
+            let request = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            
+            // insert json data to the request
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = jsonData
+            
+            
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data, response, error in
+                if error != nil{
+                    print("Error -> \(error)")
+                    return
+                }
+                
+                do {
+                    let result = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String:AnyObject]
+                    
+                    print("Result -> \(result)")
+                    
+                } catch {
+                    print("Error -> \(error)")
+                }
+            }
+            task.resume()
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    func jsonToString(json: AnyObject) -> String {
+        do {
+            let data1 =  try NSJSONSerialization.dataWithJSONObject(json, options: NSJSONWritingOptions.PrettyPrinted) // first of all convert json to the data
+            let convertedString = String(data: data1, encoding: NSUTF8StringEncoding) // the data will be converted to the string
+            print(convertedString)
+            return(convertedString!) // <-- here is ur string
+            
+        } catch let myJSONError {
+            return "";
+        }
+    }
+    
 }
 
