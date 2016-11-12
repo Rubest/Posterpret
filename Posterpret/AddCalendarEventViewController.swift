88927d328d77dc2ddf375f102a20b4eeac74c5fe
Rubest <ruban_hussain@brown.edu>
2016-11-12 09:34:47 -0500

    I think basic events are being added!

--- Posterpret/AddCalendarEventViewController.swift
+++ Posterpret/AddCalendarEventViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import EventKit
 
 class AddCalendarEventViewController: UIViewController {
+    
+    
+    @IBOutlet weak var addToCalButton: UIButton!
     
     
     override func viewDidLoad() {
@@ -17,6 +21,48 @@ class AddCalendarEventViewController: UIViewController {
     }
     
     //https://www.andrewcbancroft.com/2016/06/02/creating-calendar-events-with-event-kit-and-swift/ 
+    
+    
+    
+    @IBAction func addtoCalendarClicked(sender: AnyObject) {
+        
+        let eventStore = EKEventStore()
+        
+        eventStore.requestAccessToEntityType( EKEntityType.Event, completion:{(granted, error) in
+            
+            if (granted) && (error == nil) {
+                print("granted \(granted)")
+                print("error \(error)")
+                
+                let event = EKEvent(eventStore: eventStore)
+                
+                event.title = "Event Title"
+                event.startDate = NSDate()
+                event.endDate = NSDate()
+                event.notes = "Event Details Here"
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                
+                var event_id = ""
+                do{
+                    try eventStore.saveEvent(event, span: .ThisEvent)
+                    event_id = event.eventIdentifier
+                }
+                catch let error as NSError {
+                    print("json error: \(error.localizedDescription)")
+                }
+                
+                if(event_id != ""){
+                    print("event added !")    
+                }
+            }
+        })
+    }
+    
+    
+    
+    
+    
+    
     
 }
 
